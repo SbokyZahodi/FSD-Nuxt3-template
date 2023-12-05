cbfa1a10b426cd46de3814c05471707b86665671
injurka <xsolncu@gmail.com>
2023-12-05 16:57:19 +0400

    update lint config

--- routes/index.vue
+++ routes/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-screen h-screen bg-[#18191a] flex justify-center items-center">
-    <div class="w-100 text-white text-4xl font-bold text-center flex justify-center items-center">
+    <div class="flex justify-center items-center w-100 text-white text-4xl font-bold text-center">
       Hello World
     </div>
   </div>
