d13ef5626f5cfe88b3d9ed4b1df8bb32bcc0fdc8
injurka <102309602+injurka@users.noreply.github.com>
2023-12-06 08:59:27 +0300

    Updated config files (#1)
    * update lint config
    
    * remove unnecessary await

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import antfu from '@antfu/eslint-config'
-import unocss from '@unocss/eslint-config/flat'
 
-export default [
-  ...antfu(),
-  unocss,
-]
+export default antfu({
+  unocss: true,
+})
